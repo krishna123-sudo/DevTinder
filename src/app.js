6c073d9566b9d564c15c512d7252497fd5ed80f7
krishna sekhar <krishna@hyperx2.ai>
2026-03-01 02:45:01 +0530

    update,delete api

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 //midlleware to get the data and conver to json
 app.use(express.json());
 
-
+//create user
 app.post("/signup", async (req, res) => {
     const userObj = req.body;
     const user = new User(userObj)
@@ -24,7 +24,7 @@ app.post("/signup", async (req, res) => {
 
 })
 
-//get all the user from the database
+//get  user from the database on the basis of query
 app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
     try {
@@ -40,7 +40,19 @@ app.get("/user", async (req, res) => {
         res.status(400).send("Something went wrong");
     }
 })
+//get user by userid
+app.get("/user/:id", async (req, res) => {
+    const userId = req.params.id;
 
+    try {
+        const getUserById = await User.findById({ _id: userId });
+        res.send(getUserById);
+    } catch (error) {
+        res.status(404).send("user id not found");
+    }
+
+})
+//get all user
 app.get("/feed", async (req, res) => {
     try {
         const users = await User.find({});
@@ -49,6 +61,30 @@ app.get("/feed", async (req, res) => {
         res.status(404).send("No user avaialable")
     }
 })
+//delete user by user id
+app.delete("/user", async (req, res) => {
+    const userId = req.body.id;
+    try {
+        await User.findByIdAndDelete({ _id: userId })
+        res.send("User deleted successfully");
+    } catch (err) {
+        res.status(400).send("UserId not found to delete")
+    }
+})
+//update user by user id
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.id;
+    console.log(userId)
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate({ _id: userId }, { data }, { returnDocument: "after" })
+        res.send("user updated sucessfully");
+    } catch (err) {
+        res.status(404).send("user id not find to update")
+    }
+})
+
 
 connectDB().then(() => {
     console.log("Database connected Sucessfully")
