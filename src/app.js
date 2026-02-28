776a0dcc9616e15eaa611e07475c9adbfd3c4538
krishna sekhar <krishna@hyperx2.ai>
2026-03-01 01:36:03 +0530

    get api

--- src/app.js
+++ src/app.js
@@ -24,6 +24,31 @@ app.post("/signup", async (req, res) => {
 
 })
 
+//get all the user from the database
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.findOne({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(400).send("user Not found")
+        } else {
+            res.send(users);
+
+        }
+
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(404).send("No user avaialable")
+    }
+})
 
 connectDB().then(() => {
     console.log("Database connected Sucessfully")
